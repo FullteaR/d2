12b81a8c6d2f8b9b19702b4bab266fa673b76690
Rikuta Furuta <rikuta@furutan.com>
2019-01-14 14:45:39 +0900

    fix: tiny things

--- save.py
+++ save.py
@@ -8,26 +8,27 @@ import json
 auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
 auth.set_access_token(myAccessToken, myAccessTokenSecret)
 api = tweepy.API(auth)
-since = 1082469419694010368
+since = 1084447048613519360
 
 
 while True:
-    tl = api.home_timeline(since_id=since, count=1000)[::-1]
-    if len(tl)>0:
-        today = datetime.datetime.today()
-        year = today.year
-        month = today.month
-        day = today.day
-        path = os.path.join("data", "{0}-{1:02}-{2:02}".format(year, month, day))
-        os.makedirs(path, exist_ok=True)
+    try:
+        tl = api.home_timeline(since_id=since, count=1000)[::-1]
+        if len(tl)>0:
+            today = datetime.datetime.today()
+            year = today.year
+            month = today.month
+            day = today.day
+            path = os.path.join("data", "{0}-{1:02}-{2:02}".format(year, month, day))
+            os.makedirs(path, exist_ok=True)
 
-        for tweet in tl:
-            with open(os.path.join(path, "{}.txt".format(tweet.user.id)), "a+") as fp:
-                fp.write(tweet.id_str)
-                fp.write("\t")
-                fp.write(json.dumps(tweet._json,ensure_ascii=False))
-                fp.write("\n")
-        since = tl[-1].id
-
-    print(since)
+            for tweet in tl:
+                with open(os.path.join(path, "{}.txt".format(tweet.user.id)), "a+") as fp:
+                    fp.write(tweet.id_str)
+                    fp.write("\t")
+                    fp.write(json.dumps(tweet._json,ensure_ascii=False))
+                    fp.write("\n")
+            since = tl[-1].id
+    except:
+        pass
     time.sleep(15 * 60 / 14)
